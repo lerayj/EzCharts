48ca0038ff29dd1428fe78fec8f1f97623b3055e
Julien LERAY <leray.jul@gmail.com>
2015-09-07 17:45:18 +0200

    V0.3

--- ez-charts/ez-charts.js
+++ ez-charts/ez-charts.js
@@ -1,6 +1,10 @@
-var defaultBarIdx = 0;
-var defaultLineIdx = 0;
-var defaultDonutIdx = 0;
+var indexCore = 
+{
+  defaultBarIdx : 0,
+  defaultLineIdx : 0,
+  defaultDonutIdx : 0
+}
+
 Template.EzChartBar.helpers({
   "name": function(){
     return this.name;
@@ -9,57 +13,26 @@ Template.EzChartBar.helpers({
 
 function drawerChoiceCore(classType, dataAttName, d3Type, Idx, data){
   var tpl = Template.instance();
-  tpl.$(classType).attr(dataAttName, Idx);
-  var chart = d3.select('[' + dataAttName + '="' + Idx + '"]')
+  tpl.$(classType).attr(dataAttName, indexCore[Idx]);
+  var chart = d3.select('[' + dataAttName + '="' + indexCore[Idx] + '"]')
         .append('svg')
           .attr('width', data.width)
           .attr('height', data.height)
           .chart(d3Type);
   chart.draw(data.data);
-  Idx++;
+  indexCore[Idx]++;
   return true;
 }
 
 
 Template.EzChartBar.rendered = function() {
-  drawerChoiceCore(".chartBar", "data-ezc-bar-id", 'verticalBar', defaultBarIdx, this.data);
+  drawerChoiceCore(".chartBar", "data-ezc-bar-id", 'verticalBar', "defaultBarIdx", this.data);
 }
 
-// Template.EzChartBar.rendered = function() {
-//   var tpl = Template.instance();
-//   tpl.$(".chartBar").attr("data-ezc-bar-id", defaultBarIdx);
-//  	var chart = d3.select('[data-ezc-bar-id="' + defaultBarIdx + '"]')
-//         .append('svg')
-//           .attr('width', this.data.width)
-//           .attr('height', this.data.height)
-//           .chart('verticalBar');
-//   chart.draw(this.data.data);
-//   defaultBarIdx++;
-// 	return ("data");
-// }
-
 Template.EzChartLine.rendered = function() {
-  var tpl = Template.instance();
-  tpl.$(".chartLine").attr("data-ezc-line-id", defaultLineIdx);
-  var chart = d3.select('[data-ezc-line-id="' + defaultLineIdx + '"]')
-        .append('svg')
-          .attr('width', this.data.width)
-          .attr('height', this.data.height)
-          .chart('genericLines');
-  chart.draw(this.data.data);
-	defaultLineIdx++;
-  return ("data");
+  drawerChoiceCore(".chartLine", "data-ezc-line-id", 'genericLines', "defaultLineIdx", this.data);
 }
 
 Template.EzChartDonut.rendered = function() {
-  var tpl = Template.instance();
-  tpl.$(".chartDonut").attr("data-ezc-donut-id", defaultDonutIdx);
-  var chart = d3.select('[data-ezc-donut-id="' + defaultDonutIdx + '"]')
-        .append('svg')
-          .attr('width', this.data.width)
-          .attr('height', this.data.height)
-          .chart('genericDonut');
-  chart.draw(this.data.data);
-  defaultDonutIdx++;
-	return ("data");
+  drawerChoiceCore(".chartDonut", "data-ezc-donut-id", 'genericDonut', "defaultDonutIdx", this.data);
 }
