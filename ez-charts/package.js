d0118963f8915e23e253b7acf08f174a28b165ba
Julien LERAY <leray.jul@gmail.com>
2015-09-14 14:42:48 +0200

    rename

--- ez-charts/package.js
+++ ez-charts/package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'easy to use generics SVG charts generator',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/lerayj/EzCharts.git',
+  git: 'https://github.com/lerayj/EzChartsForMeteor.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
